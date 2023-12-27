53d6737c84433e866123d26d89bbf46f6559b913
letuan279 <latuan388@gmail.com>
2023-12-27 09:58:19 +0700

    [BE] API CRUD nhóm

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const userController = require('../controllers/user.controller');
+const groupController = require('../controllers/group.controller');
+const memberController = require('../controllers/member.controller');
 const router = express.Router();
 
 // user
 router.get('/user', userController.getAll);
 router.post('/login', userController.login);
+
+// group
+router.get('/group/:idUser', groupController.getAll);
+router.post('/group/add', groupController.add);
 
 module.exports = router
